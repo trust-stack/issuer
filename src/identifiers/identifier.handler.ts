771e8e6143f7dc06fc20e133271a51f635fbe583
Harley Thomas <harleyjackthomas@gmail.com>
2025-09-29 06:44:55 +0930

    Updating lockfile.

--- src/identifiers/identifier.handler.ts
+++ src/identifiers/identifier.handler.ts
@@ -1,32 +1,28 @@
-import {OpenAPIHono} from "@hono/zod-openapi";
+import { OpenAPIHono } from '@hono/zod-openapi';
+import { IdentifierNotFoundError } from './identifier.errors';
 import {
   createIdentifierRoute,
   getIdentifierRoute,
   updateIdentifierRoute,
-} from "./identifier.route";
-import {
-  createIdentifier,
-  getIdentifier,
-  updateIdentifier,
-} from "./identifier.service";
-import {IdentifierNotFoundError} from "./identifier.errors";
+} from './identifier.route';
+import { createIdentifier, getIdentifier, updateIdentifier } from './identifier.service';
 
 const app = new OpenAPIHono();
 
 app.openapi(createIdentifierRoute, async (c) => {
-  const identifier = await createIdentifier(c.req.valid("json"));
+  const identifier = await createIdentifier(c.req.valid('json'));
   return c.json(identifier, 201);
 });
 
 app.openapi(getIdentifierRoute, async (c) => {
-  const {did} = c.req.valid("param");
+  const { did } = c.req.valid('param');
 
   try {
     const identifier = await getIdentifier(did);
     return c.json(identifier, 200);
   } catch (error) {
     if (error instanceof IdentifierNotFoundError) {
-      return c.json({message: error.message}, 404);
+      return c.json({ message: error.message }, 404);
     }
 
     throw error;
@@ -34,15 +30,15 @@ app.openapi(getIdentifierRoute, async (c) => {
 });
 
 app.openapi(updateIdentifierRoute, async (c) => {
-  const {did} = c.req.valid("param");
-  const payload = c.req.valid("json");
+  const { did } = c.req.valid('param');
+  const payload = c.req.valid('json');
 
   try {
     const identifier = await updateIdentifier(did, payload);
     return c.json(identifier, 200);
   } catch (error) {
     if (error instanceof IdentifierNotFoundError) {
-      return c.json({message: error.message}, 404);
+      return c.json({ message: error.message }, 404);
     }
 
     throw error;
