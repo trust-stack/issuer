1405ff734975b2c117c71160caa018e6c6ea8d6b
Harley Thomas <harleyjackthomas@gmail.com>
2025-09-30 11:55:27 +0930

    Adding build step.

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig([
+  {
+    entry: ['src/index.ts'],
+    format: ['esm'],
+    dts: true,
+    sourcemap: true,
+    clean: true,
+    outDir: 'dist',
+    target: 'node20',
+  },
+]);
